98e50dc84542fc70d27852a1ba48cad69ab052b0
Hosein Behkamal <hbehkamal@gmail.com>
2020-09-13 21:04:09 +0430

    add dashboerd and auth provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,21 @@ import * as React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
-import DynamicList from "./components/DynamicList";
-import DynamicEdit from "./components/DynamicEdit";
 import UserList from "./resources/user/user.list";
 import UserEdit from "./resources/user/user.edit";
 import UserCreate from "./resources/user/user.create";
 import Dashboard from "./components/dashboard";
+import authProvider from "./core/authProvider";
 
 const dataProvider = jsonServerProvider("http://localhost:3001");
 const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
     <Resource name="user" list={UserList} edit={UserEdit} create={UserCreate} />
-    <Resource name="student" list={DynamicList} edit={DynamicEdit} />
+    {/* <Resource name="student" list={DynamicList} edit={DynamicEdit} />
     <Resource name="master" list={DynamicList} edit={DynamicEdit} />
     <Resource name="class" list={DynamicList} edit={DynamicEdit} />
     <Resource name="department" list={DynamicList} edit={DynamicEdit} />
-    <Resource name="course" list={DynamicList} edit={DynamicEdit} />
+    <Resource name="course" list={DynamicList} edit={DynamicEdit} /> */}
   </Admin>
 );
 
